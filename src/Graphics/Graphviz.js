2d9b3c2dd36d527afc4d2eaf8c204546395c3436
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2019-06-14 08:06:45 +0200

    update to viz.js 2 and make functions async
    not sure, if Aff is the right choise.
    While the function is asynchronous, it is not side-effectful and if
    one uses DotLang package, it also does not create errors

--- src/Graphics/Graphviz.js
+++ src/Graphics/Graphviz.js
@@ -1,12 +1,16 @@
 "use strict";
+const Viz = require('viz.js');
+const viz_full = require('viz.js/full.render.js');
 
-exports.viz_internal = function(data, format, engine, scale) {
-  var v = undefined;
-  try {
-    v = require('viz.js');
+exports._viz_internal = function(engine) {
+  const viz = new Viz({ Module: viz_full.Module, render: viz_full.render });
+  return function(format) {
+    return function(string) {
+      return function(onError, onSuccess) {
+        viz.renderString(string, { engine: engine, format: format})
+          .then(onSuccess)
+          .catch(onError)
+      }
+    }
   }
-  catch(error) {
-    v = Viz;
-  }
-  return v(data, {format: format, engine: engine, scale: scale})
 }
--- src/Graphics/Graphviz.purs
+++ src/Graphics/Graphviz.purs
@@ -1,12 +1,16 @@
-module Graphics.Graphviz (renderToJson, renderToSvg, renderReprSvg, Engine(..)) where
+module Graphics.Graphviz (renderToText, renderDotLang, Engine(..), Format(..)) where
 
-import Data.DotLang (class GraphRepr, toGraph)
 import Data.DotLang.Class (class DotLang, toText)
-import Data.Function (($))
-import Data.Function.Uncurried (Fn4, runFn4)
+import Data.Function ((<<<), (>>>))
 import Data.Show (class Show, show)
+import Effect.Aff (Aff)
+import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
 
-foreign import viz_internal :: Fn4 String String String Int String
+
+-- | engine → format → dotText → Aff String
+foreign import _viz_internal ∷ String → String → String → EffectFnAff String
+
+-- foreign import _viz_render ∷ ∀a. String → String → (String → a) 
 
 data Engine
   = Circo
@@ -22,15 +26,46 @@ instance showEngine :: Show Engine where
   show Osage = "osage"
   show Twopi = "twopi"
 
+data Format
+  = Svg
+  | SimpleDot
+  | XDot
+  | Plain
+  | PlainExt
+  | Ps
+  | Ps2
+  | Json
+  | Json0
 
-renderReprSvg :: ∀a. GraphRepr a => Engine -> a -> String
-renderReprSvg e a = renderToSvg e $ toGraph a
+instance showFormat ∷ Show Format where
+  show Svg = "svg"
+  show SimpleDot = "dot"
+  show XDot = "xdot"
+  show Plain = "plain"
+  show PlainExt = "plain-ext"
+  show Ps = "ps"
+  show Ps2 = "ps2"
+  show Json = "json"
+  show Json0 = "Json0"
 
-renderToJson :: ∀a. DotLang a => Engine -> a -> String
-renderToJson e a = runFn4 viz_internal (toText a) "json" (show e) 1
+renderToText ∷ Engine → Format → String → Aff String
+renderToText engine format = fromEffectFnAff <<< (_viz_internal (show engine) (show format))
 
-renderToSvg :: ∀a. DotLang a => Engine -> a -> String
-renderToSvg e a = runFn4 viz_internal (toText a) "svg" (show e) 1
+-- renderToJson ∷ Engine → Boolean → String → Aff Foreign
+-- renderToJson engine useJson0 = fromEffectFnAff <<< (_viz_internal "renderJSONObject" (show engine) format)
+--   where format = if useJson0 then "json0" else "json"
 
-renderToXDot :: ∀a. DotLang a => Engine → a → String
-renderToXDot e a = runFn4 viz_internal (toText a) "xdot" (show e) 1
+renderDotLang ∷ ∀a. DotLang a => Engine → Format → a → Aff String
+renderDotLang engine format a = renderToText engine format (toText a)
+
+-- renderReprSvg :: ∀a. GraphRepr a => Engine -> a -> String
+-- renderReprSvg e a = renderToSvg e $ toGraph a
+
+-- renderToJson :: ∀a. DotLang a => Engine -> a -> String
+-- renderToJson e a = runFn4 viz_internal (toText a) "json" (show e) 1
+
+-- renderToSvg :: ∀a. DotLang a => Engine -> a -> String
+-- renderToSvg e a = runFn4 viz_internal (toText a) "svg" (show e) 1
+
+-- renderToXDot :: ∀a. DotLang a => Engine → a → String
+-- renderToXDot e a = runFn4 viz_internal (toText a) "xdot" (show e) 1
